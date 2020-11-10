cba5dfa88edaf2c66fdee34061292d100b327934
Yzs <yangzs8888@hotmail.com>
2020-11-10 23:37:33 +0800

    update

---
+++ designpattern/Behavioral/Strategy.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+class Strategy
+{
+public:
+    virtual void AlogrithmInterface() = 0;
+};
+
+class ConcreteStrategyA : public Strategy
+{
+public:
+    void AlogrithmInterface() override
+    {
+        printf("===ConcreteStrategyA==");
+    }
+};
+class ConcreteStrategyB : public Strategy
+{
+public:
+    void AlogrithmInterface() override
+    {
+        printf("===ConcreteStrategyB==");
+    }
+};
+
+class Context
+{
+    Strategy *strategy;
+
+public:
+    Context(Strategy *strategy_) : strategy(strategy_) {}
+    void run()
+    {
+        strategy->AlogrithmInterface();
+    }
+};
+int main()
+{
+    Context *ctx = new Context(new ConcreteStrategyA());
+    ctx->run();
+}
